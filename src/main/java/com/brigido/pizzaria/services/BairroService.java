6b4f6b84c19e651224a6be5a4f6e6e04c3ee8b43
brigido <jv_brigido@hotmail.com>
2024-09-04 19:21:57 -0400

    feat: Implement the service

---
+++ src/main/java/com/brigido/pizzaria/services/BairroService.java
@@ -0,0 +1,39 @@
+package com.brigido.pizzaria.services;
+
+import com.brigido.pizzaria.dtos.BairroDto;
+import com.brigido.pizzaria.mappers.BairroMapper;
+import com.brigido.pizzaria.models.Bairro;
+import com.brigido.pizzaria.repositories.BairroRepository;
+import com.brigido.pizzaria.validators.BairroValidator;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class BairroService {
+
+    @Autowired
+    private BairroRepository bairroRepository;
+    @Autowired
+    private BairroMapper bairroMapper;
+    @Autowired
+    private BairroValidator bairroValidator;
+
+    public List<BairroDto> findAll(){
+        return bairroRepository.findAll()
+                .stream()
+                .map(bairroMapper::toDto)
+                .collect(Collectors.toList());
+    }
+    @Transactional
+    public BairroDto create(BairroDto bairroDto){
+        bairroValidator.validateUniqueName(bairroDto.name());
+
+        Bairro bairro = bairroMapper.toEntity(bairroDto);
+        Bairro savedBairro = bairroRepository.save(bairro);
+        return bairroMapper.toDto(savedBairro);
+    }
+}
