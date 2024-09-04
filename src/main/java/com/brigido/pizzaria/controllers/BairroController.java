888fff1ed8b045898bfb886b11a839e10b7b1eb8
brigido <jv_brigido@hotmail.com>
2024-09-04 19:21:15 -0400

    feat: Implement the controller

---
+++ src/main/java/com/brigido/pizzaria/controllers/BairroController.java
@@ -0,0 +1,29 @@
+package com.brigido.pizzaria.controllers;
+
+import com.brigido.pizzaria.dtos.BairroDto;
+import com.brigido.pizzaria.services.BairroService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/bairros")
+public class BairroController {
+
+    @Autowired
+    private BairroService bairroService;
+
+    @GetMapping
+    public List<BairroDto> findALl(){
+     return bairroService.findAll();
+    }
+    @PostMapping
+    public ResponseEntity<BairroDto> createBairro(@Valid @RequestBody BairroDto bairroDto){
+        BairroDto createdBairroDto = bairroService.create(bairroDto);
+        return new ResponseEntity<>(createdBairroDto, HttpStatus.CREATED);
+    }
+}
