28595a0fb04584b95af3e5afc9a2fd0b065122c0
brigido <jv_brigido@hotmail.com>
2024-09-04 22:25:40 -0400

    feat: Add the validationAndReturnsExistingBairro method

--- src/main/java/com/brigido/pizzaria/validators/BairroValidator.java
+++ src/main/java/com/brigido/pizzaria/validators/BairroValidator.java
@@ -1,6 +1,8 @@
 package com.brigido.pizzaria.validators;
 
 import com.brigido.pizzaria.exceptions.BairroAlreadyExistsException;
+import com.brigido.pizzaria.exceptions.BairroNotFoundException;
+import com.brigido.pizzaria.models.Bairro;
 import com.brigido.pizzaria.repositories.BairroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,11 +14,12 @@ public class BairroValidator {
     private BairroRepository bairroRepository;
 
     public void validateUniqueName(String name) {
-        var existingBairro = bairroRepository.findByName(name);
-
-        if(existingBairro != null){
-            throw new BairroAlreadyExistsException("Bairro com o nome '" +
-                    name + "' já existe.");
-        }
+        bairroRepository.findByName(name).ifPresent(bairro -> {
+            throw new BairroAlreadyExistsException("Bairro com o nome '" + name + "' já existe.");
+        });
+    }
+    public Bairro validateAndReturnExistingBairro(String name) {
+        return bairroRepository.findByName(name)
+                .orElseThrow(() -> new BairroNotFoundException("Bairro com o nome '" + name + "' não foi encontrado."));
     }
 }
