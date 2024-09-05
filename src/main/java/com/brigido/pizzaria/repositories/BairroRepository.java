db0164cc094d2b6867b6d7d7cc71d951a341be02
brigido <jv_brigido@hotmail.com>
2024-09-04 22:24:28 -0400

    update: Update the type of findByName method for Optional

--- src/main/java/com/brigido/pizzaria/repositories/BairroRepository.java
+++ src/main/java/com/brigido/pizzaria/repositories/BairroRepository.java
@@ -2,7 +2,10 @@ package com.brigido.pizzaria.repositories;
 
 import com.brigido.pizzaria.models.Bairro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface BairroRepository extends JpaRepository<Bairro, Long> {
-    Bairro findByName(String name);
+    Optional<Bairro> findByName(String name);
 }
