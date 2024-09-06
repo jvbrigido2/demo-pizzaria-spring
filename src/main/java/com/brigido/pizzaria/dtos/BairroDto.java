e6e1597ac0256a80ccc5de24140e43d6f291dcd9
brigido <jv_brigido@hotmail.com>
2024-09-06 16:19:16 -0400

    fix: Adiciona o id no BairroDto para o front conseguir utiliza-lo

--- src/main/java/com/brigido/pizzaria/dtos/BairroDto.java
+++ src/main/java/com/brigido/pizzaria/dtos/BairroDto.java
@@ -5,7 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-public record BairroDto(@NotBlank(message = "O nome do bairro não pode ser vazio")
+public record BairroDto(Long id,
+                        @NotBlank(message = "O nome do bairro não pode ser vazio")
                         String name,
                         @NotNull(message = "A taxa não pode ser nula")
                         @Positive(message = "A taxa deve ser um valor positivo")
