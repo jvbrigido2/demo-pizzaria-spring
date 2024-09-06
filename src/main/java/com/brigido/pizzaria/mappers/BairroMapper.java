f5bc7efe2193536a66fd2af1d50ce0cbed279c44
brigido <jv_brigido@hotmail.com>
2024-09-06 16:19:40 -0400

    feat: Adiciona o BairroCreateDto ao Mapper

--- src/main/java/com/brigido/pizzaria/mappers/BairroMapper.java
+++ src/main/java/com/brigido/pizzaria/mappers/BairroMapper.java
@@ -1,5 +1,6 @@
 package com.brigido.pizzaria.mappers;
 
+import com.brigido.pizzaria.dtos.BairroCreateDto;
 import com.brigido.pizzaria.dtos.BairroDto;
 import com.brigido.pizzaria.models.Bairro;
 import org.mapstruct.Mapper;
@@ -7,6 +8,7 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface BairroMapper {
+    Bairro toEntity(BairroCreateDto bairroCreateDto);
     BairroDto toDto(Bairro bairro);
     Bairro toEntity(BairroDto bairroDto);
 }
