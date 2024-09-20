b255e68ded252f1c7d2924e595e1756d19d352d1
jvbrigido2 <jv_brigido@hotmail.com>
2024-09-20 03:10:59 -0300

    feat: add UpdateBairroRequest

---
+++ src/main/java/com/brigido/pizzaria/dtos/UpdateBairroRequest.java
@@ -0,0 +1,17 @@
+package com.brigido.pizzaria.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.With;
+
+@With
+public record UpdateBairroRequest(
+        @NotBlank(message = "O nome do bairro não pode ser vazio")
+        @Size(min = 3 , max = 100, message = "O nome tem que possuir entre 5 a 100 caracteres")
+        String name,
+        @NotNull(message = "A taxa não pode ser nula")
+        @Positive(message = "A taxa deve ser um valor positivo")
+        double tax
+) { }
